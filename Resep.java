d40e4692839365ab4e633fb85fe558b8338dd7b2
Mch4liem <113653585+Mch4liem@users.noreply.github.com>
2022-10-06 10:52:56 +0700

    inisiasi resep kraby patty

--- Resep.java
+++ Resep.java
@@ -1,3 +1,8 @@
 public class Resep{
-    println {""}
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby patty");
+        System.out.println("-------------------");
+        System.out.println("1. Daging");
+        System.out.println("2. Bawang");
+    }
 };
